87549f02d441743b8df8c2181f00d9c71b77f7a6
Sejan~ <md4sejan@gmail.com>
2024-08-25 01:59:07 +0600

    product card animation added and helmet

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -4,6 +4,7 @@ import "rc-slider/assets/index.css";
 import Slider from "rc-slider";
 import Rating from "../components/Rating";
 import Loading from "../components/Loading";
+import { Helmet } from "react-helmet-async";
 
 const ProductsPage = () => {
   const [priceRange, setPriceRange] = useState([0, 1000]);
@@ -84,81 +85,110 @@ const ProductsPage = () => {
   }, []);
 
   return (
-    <div className="container mx-auto px-4">
-      <div className="grid grid-cols-1 lg:grid-cols-4 gap-6 py-20">
-        {/* Left Column - Filters */}
-        <div className="lg:col-span-1">
-          <div className="flex flex-col gap-6 p-4 border rounded-md lg:sticky top-8">
-            <input
-              type="text"
-              placeholder="Search by name, brand..."
-              className="p-2 border rounded-md"
-            />
+    <>
+      <Helmet>
+        <title>Products - MechKeyMart</title>
+      </Helmet>
 
-            <div className="px-4 py-4">
-              <label className="block mb-2">
-                Price Range: ${priceRange[0]} - ${priceRange[1]}
-              </label>
-              <div className="pe-4">
-                <Slider
-                  range
-                  min={0}
-                  max={1000}
-                  value={priceRange}
-                  onChange={handlePriceRangeChange}
-                  trackStyle={[{ backgroundColor: "#4A90E2" }]}
-                  handleStyle={[
-                    { borderColor: "#4A90E2" },
-                    { borderColor: "#4A90E2" },
-                  ]}
-                />
-              </div>
-            </div>
-            <select className="p-2 border rounded-md">
-              <option value="">Sort by</option>
-              <option value="low-to-high">Price: Low to High</option>
-              <option value="high-to-low">Price: High to Low</option>
-            </select>
-            <button className="p-2 bg-red-500 text-white rounded-md">
-              Clear Filters
-            </button>
-          </div>
-        </div>
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-1 lg:grid-cols-4 gap-6 py-20">
+          {/* Left Column - Filters */}
+          <div className="lg:col-span-1">
+            <div className="flex flex-col gap-6 p-4 border rounded-md lg:sticky top-8">
+              <input
+                type="text"
+                placeholder="Search by name, brand..."
+                className="p-2 border rounded-md"
+              />
 
-        {/* Right Column - Products */}
-        <div className="lg:col-span-3">
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {loading ? (
-              <Loading />
-            ) : (
-              filteredProducts.map((product) => (
-                <div key={product.id} className="border p-4 rounded-lg">
-                  <img
-                    src={product.image}
-                    alt={product.name}
-                    className="w-full h-48 object-cover rounded-md mb-4"
+              <div className="px-4 py-4">
+                <label className="block mb-2">
+                  Price Range: ${priceRange[0]} - ${priceRange[1]}
+                </label>
+                <div className="pe-4">
+                  <Slider
+                    range
+                    min={0}
+                    max={1000}
+                    value={priceRange}
+                    onChange={handlePriceRangeChange}
+                    trackStyle={[{ backgroundColor: "#4A90E2" }]}
+                    handleStyle={[
+                      { borderColor: "#4A90E2" },
+                      { borderColor: "#4A90E2" },
+                    ]}
                   />
-                  <h2 className="text-lg font-bold mb-3">{product.name}</h2>
-                  <div className="flex flex-col gap-1">
-                    <p>Brand: {product.brand}</p>
-                    <p>Available Quantity: {product.quantity}</p>
-                    <p>Price: ${product.price}</p>
-
-                    <Rating rating={Math.round(product.rating)} />
-                  </div>
-                  <Link
-                    to={`/product/${product.id}`}
-                    className="inline-block px-4 py-1 mt-6 text-white bg-blue-500 rounded-md hover:bg-blue-600 transition-colors duration-200"
-                  >
-                    Show Details
-                  </Link>
                 </div>
-              ))
-            )}
+              </div>
+              <select className="p-2 border rounded-md">
+                <option value="">Sort by</option>
+                <option value="low-to-high">Price: Low to High</option>
+                <option value="high-to-low">Price: High to Low</option>
+              </select>
+              <button className="p-2 bg-red-500 text-white rounded-md">
+                Clear Filters
+              </button>
+            </div>
+          </div>
+
+          {/* Right Column - Products */}
+          <div className="lg:col-span-3">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {loading ? (
+                <Loading />
+              ) : (
+                filteredProducts.map((product) => (
+                  <div
+                    key={product.id}
+                    className="productcard border p-4 rounded-lg"
+                  >
+                    <figure className="w-full h-48 rounded-md  overflow-hidden mb-4">
+                      <img
+                        src={product.image}
+                        alt={product.name}
+                        className="w-full h-48 object-cover"
+                      />
+                    </figure>
+
+                    <h2 className="text-lg font-bold mb-3">{product.name}</h2>
+                    <div className="flex flex-col gap-1">
+                      <p>Brand: {product.brand}</p>
+                      <p>Available Quantity: {product.quantity}</p>
+                      <p>Price: ${product.price}</p>
+
+                      <Rating rating={Math.round(product.rating)} />
+                    </div>
+                    <Link
+                      to={`/product/${product.id}`}
+                      className="inline-block px-3 py-1 mt-6 text-white bg-blue-500 rounded-md hover:bg-blue-600 transition-colors duration-200 morebtn"
+                    >
+                      <span className="flex items-center">
+                        Details
+                        <svg
+                          className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 14 10"
+                        >
+                          <path
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M1 5h12m0 0L9 1m4 4L9 9"
+                          />
+                        </svg>
+                      </span>
+                    </Link>
+                  </div>
+                ))
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
