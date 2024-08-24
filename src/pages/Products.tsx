641d95963c1f742b4ec65476f48411edb40ec92c
Sejan~ <md4sejan@gmail.com>
2024-08-25 00:33:07 +0600

    Rating component

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -2,8 +2,7 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import "rc-slider/assets/index.css";
 import Slider from "rc-slider";
-import { FaStar, FaRegStar } from "react-icons/fa";
-import Rating from "react-rating";
+import Rating from "../components/Rating";
 
 const ProductsPage = () => {
   const [priceRange, setPriceRange] = useState([0, 1000]);
@@ -15,7 +14,7 @@ const ProductsPage = () => {
       brand: "Cherry",
       quantity: 50,
       price: 120,
-      rating: 4.5,
+      rating: 3.5,
       image:
         "https://i.pinimg.com/564x/fd/42/6b/fd426b4705f3f93210606453b7659f4e.jpg",
     },
@@ -25,7 +24,7 @@ const ProductsPage = () => {
       brand: "Logitech",
       quantity: 30,
       price: 150,
-      rating: 4.7,
+      rating: 2.7,
       image:
         "https://i.pinimg.com/564x/80/83/b9/8083b918f9f09560a04c2e8501d763f6.jpg",
     },
@@ -35,7 +34,7 @@ const ProductsPage = () => {
       brand: "Corsair",
       quantity: 20,
       price: 180,
-      rating: 4.8,
+      rating: 1.8,
       image:
         "https://i.pinimg.com/564x/6d/5e/35/6d5e3544bcf4072c747ecdfe26e9fe6a.jpg",
     },
@@ -55,7 +54,7 @@ const ProductsPage = () => {
       brand: "SteelSeries",
       quantity: 15,
       price: 200,
-      rating: 4.9,
+      rating: 4.2,
       image:
         "https://i.pinimg.com/564x/26/06/da/2606dadb35bf67e86111409cff6e5866.jpg",
     },
@@ -65,7 +64,7 @@ const ProductsPage = () => {
       brand: "Ducky",
       quantity: 40,
       price: 100,
-      rating: 4.4,
+      rating: 4.1,
       image:
         "https://i.pinimg.com/564x/3c/c0/c1/3cc0c15c0ad52c149eea35f2e980c6a3.jpg",
     },
@@ -88,34 +87,31 @@ const ProductsPage = () => {
       <div className="grid grid-cols-1 lg:grid-cols-4 gap-6 py-20">
         {/* Left Column - Filters */}
         <div className="lg:col-span-1">
-          <div className="flex flex-col gap-4 p-4 border rounded-md">
+          <div className="flex flex-col gap-6 p-4 border rounded-md lg:sticky top-8">
             <input
               type="text"
-              placeholder="Search by name..."
+              placeholder="Search by name, brand..."
               className="p-2 border rounded-md"
             />
-            <input
-              type="text"
-              name="brand"
-              placeholder="Filter by brand..."
-              className="p-2 border rounded-md"
-            />
-            <div>
+
+            <div className="px-4 py-4">
               <label className="block mb-2">
                 Price Range: ${priceRange[0]} - ${priceRange[1]}
               </label>
-              <Slider
-                range
-                min={0}
-                max={1000}
-                value={priceRange}
-                onChange={handlePriceRangeChange}
-                trackStyle={[{ backgroundColor: "#4A90E2" }]}
-                handleStyle={[
-                  { borderColor: "#4A90E2" },
-                  { borderColor: "#4A90E2" },
-                ]}
-              />
+              <div className="pe-4">
+                <Slider
+                  range
+                  min={0}
+                  max={1000}
+                  value={priceRange}
+                  onChange={handlePriceRangeChange}
+                  trackStyle={[{ backgroundColor: "#4A90E2" }]}
+                  handleStyle={[
+                    { borderColor: "#4A90E2" },
+                    { borderColor: "#4A90E2" },
+                  ]}
+                />
+              </div>
             </div>
             <select className="p-2 border rounded-md">
               <option value="">Sort by</option>
@@ -141,21 +137,17 @@ const ProductsPage = () => {
                     alt={product.name}
                     className="w-full h-48 object-cover rounded-md mb-4"
                   />
-                  <h2 className="text-lg font-bold">{product.name}</h2>
-                  <p>Brand: {product.brand}</p>
-                  <p>Available Quantity: {product.quantity}</p>
-                  <p>Price: ${product.price}</p>
-                  <div className="flex items-center">
-                    <Rating
-                      initialRating={product.rating}
-                      readonly
-                      emptySymbol={<FaRegStar color="#FFD700" />}
-                      fullSymbol={<FaStar color="#FFD700" />}
-                    />
+                  <h2 className="text-lg font-bold mb-3">{product.name}</h2>
+                  <div className="flex flex-col gap-1">
+                    <p>Brand: {product.brand}</p>
+                    <p>Available Quantity: {product.quantity}</p>
+                    <p>Price: ${product.price}</p>
+
+                    <Rating rating={Math.round(product.rating)} />
                   </div>
                   <Link
                     to={`/product/${product.id}`}
-                    className="inline-block px-4 py-1 mt-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 transition-colors duration-200"
+                    className="inline-block px-4 py-1 mt-6 text-white bg-blue-500 rounded-md hover:bg-blue-600 transition-colors duration-200"
                   >
                     Show Details
                   </Link>
